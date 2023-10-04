2d9b91ff20955b6d189bf5b156ec5d2e43b88df1
Manas <manasgedam26@gmail.com>
2023-10-04 22:23:59 +0530

    will look into it after some time

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-require('dotenv').config()
+// require('dotenv').config()
+// console.log(process.env);
 const express = require("express"); // here the express getting imported, this is a different way of importing in JS 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.get("/", (req, res) => { //this is call back
+  res.render("index")
+});
+
+app.get("/login", (req, res) => { //this is call back
   res.send("Hello World!");
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(5000, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-app.get("/youtube", (req,res)=> {
-  res.send("This is youtube");
-})
-
-app.get("/login", (req, res) => {
-    res.send("<h1>Hi you can login here</h1>");
-})
